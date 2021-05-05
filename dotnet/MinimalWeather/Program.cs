bbe40a21d6b75e27ca5790ee68a5357615888aeb
ladynaggaga <maria.naggaga@live.ca>
2021-05-05 17:46:39 -0400

    BUILD 2021 - Minimal APIs

--- dotnet/MinimalWeather/Program.cs
+++ dotnet/MinimalWeather/Program.cs
@@ -23,7 +23,8 @@ app.MapGet("/weather/{location}", [EnableCors("weather")] async (Coordinate loca
     {
         CurrentWeather = currentQuery.Result.Results[0],
         HourlyForecasts = hourlyQuery.Result.Forecasts,
-        DailyForecasts = dailyQuery.Result.Forecasts,   
+        DailyForecasts = dailyQuery.Result.Forecasts, 
+        Debug = "Hi",
     };
 });
 
